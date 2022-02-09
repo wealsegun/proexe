45e57108ed488508654001f125ccf7ece54efd65
Olawale OGUNLEYE <Olawale.OGUNLEYE@NSIAINSURANCE.COM>
2022-02-09 01:06:04 +0100

    architectural structure

---
+++ src/redux/actions/actionTypes.jsx
@@ -0,0 +1,8 @@
+export const TASK_LIST_STATE = 'TaskListState';
+export const TASK_CREATION_STATE ="TaskCreationState";
+export const TASK_CREATION_SUCCESS ="TaskCreationSuccess";
+export const DELETE_TASK_STATE  = "DeleteTaskState";
+export const DELETE_TASK_SUCCESS  = "DeleteTaskSuccess";
+export const GET_TASK_STATE = "GetTaskState";
+export const UPDATE_TASK_STATE = "UpdateTaskState";
+export const UPDATE_TASK_SUCCESS = "UpdateTaskState";
